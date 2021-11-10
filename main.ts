633c6c07052b412afdd5d4180809641e00357508
Thom Johansen <thomj@notam.no>
2021-11-10 03:59:54 +0100

    These should probs be empty.

--- main.ts
+++ main.ts
@@ -1,12 +1,1 @@
-radio.onReceivedString(function (receivedString) {
-    if (receivedString.includes("plonk")) {
-        orchestra.setFreq(200)
-    }
-})
-radio.setGroup(1)
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        orchestra.setFreq(220)
-        basic.pause(100)
-    }
-})
+
